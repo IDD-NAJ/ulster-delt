9812b078542f30350a2836ece92dd03b7a2d4cfb
IDD-NAJ <Najeebiddrisu79@gmail.com>
2025-04-29 10:30:00 -0700

    Update Next.js configuration for proper module resolution

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
   output: 'standalone',
   images: {
     domains: ['images.unsplash.com', "avatars.githubusercontent.com"],
